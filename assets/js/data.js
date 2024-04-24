6423c0b0db810f0106bb1d513ed4f6673d0c5a1a
robertovmartins <163680676+robertovmartins@users.noreply.github.com>
2024-04-24 09:23:37 -0300

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -48,5 +48,15 @@ var produtos = [
         price: 120.00,
         image: "https://puffignite.co/cdn/shop/files/V80_BlackEdition_SinglePackDevice_0009_grapeice_2000x.jpg?v=1707934808",
         disponivel: true
+    },
+    {
+        id: "6",
+        marca: "Ignite",
+        title: "Pod Descartável Ignite V80 Watermelon Ice",
+        description: "Nossa mais recente caneta V80 Vape descartável apresenta hardware totalmente novo e tecnologia de desenho automático para oferecer a experiência vape mais suave sem carregar ou qualquer ajuste de configuração. Basta inspirar para ativar o dispositivo pré-cheio. A bateria de 500mAh oferece um sabor delicioso em até 8.000 baforadas, o suficiente para mantê-lo satisfeito por dias. Cada caneta contém 5% de nicotina derivada do tabaco para uma experiência agradável estilo cigarro.",
+        puffs: 8000,
+        price: 120.00,
+        image: "https://puffignite.co/cdn/shop/files/V80_BlackEdition_SinglePackDevice_0000_WATERMELONICE_5000x.jpg?v=1713918859",
+        disponivel: true
     }
     ]
